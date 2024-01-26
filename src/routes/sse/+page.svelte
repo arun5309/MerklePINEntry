237d11fa489275b6c535bda2e6e180b3fe4e8809
Arunachalaeshwaran V R <avr5309@gmail.com>
2024-01-26 09:29:28 +0530

    Made SSE working example

---
+++ src/routes/sse/+page.svelte
@@ -0,0 +1,21 @@
+<script>    	
+    var redirect = false;
+    const evtSource = new EventSource(`http://${import.meta.env.VITE_LOCAL_IP}:8000/stream`);
+    evtSource.onmessage = function(event) {
+    	if(event.data == "rick roll") {
+    	    redirect = true;
+    	    // window.location.replace(import.meta.env.VITE_RR);
+    	    window.location.href = import.meta.env.VITE_RR;
+    	}
+    	else {
+    	    redirect = false;
+    	}
+    };
+</script>
+
+
+<h1>SSE Test</h1>
+
+{#if redirect}
+	<p>Redirect!</p>
+{/if}
