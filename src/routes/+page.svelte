6b1f08da7168d59da88768dc56f2b2a75d8f9aa8
Arunachalaeshwaran V R <avr5309@gmail.com>
2024-01-29 21:46:14 +0530

    Fixed formatting issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -102,8 +102,8 @@
 	// Code for UI
 	let digits = '';
 	let started = false;
-	let color_odd = "#ffffff";
-	let color_even = "cyan";
+	let color_odd = '#ffffff';
+	let color_even = 'cyan';
 
 	function padNum(num: number) {
 		return num.toString().padStart(2, '0');
@@ -163,7 +163,7 @@
 <svelte:head>
 	<title>Merkle PIN Entry</title>
 	<meta name="description" content="A cognitive trapdoor game based on Merkle puzzles" />
-	<link rel="preconnect" href={`http://${import.meta.env.VITE_LOCAL_IP}:8000/`}>
+	<link rel="preconnect" href={`http://${import.meta.env.VITE_LOCAL_IP}:8000/`} />
 </svelte:head>
 
 <h1>Merkle PIN Entry</h1>
@@ -172,13 +172,13 @@
 	<!-- <video src={url} autoplay></video> -->
 	<h2>Redirected!</h2>
 {:else if !started}
-	<button on:click={() => started=true}>Start Game</button>
+	<button on:click={() => (started = true)}>Start Game</button>
 {:else if cur_step == 4}
 	{#if show_pin}
 		<div>Entered PIN: {pin}</div>
 	{/if}
 	<br />
-	<button on:click={() => show_pin = !show_pin}>Toggle PIN Visibility</button>
+	<button on:click={() => (show_pin = !show_pin)}>Toggle PIN Visibility</button>
 {:else if is_entry_phase == false}
 	<!-- modify next two lines to make clicking phase transition -->
 	<!-- on:click={() => (is_entry_phase = true)} -->
@@ -253,10 +253,10 @@
 	}
 
 	.entry:nth-child(odd) {
-	    background: var(--color-odd);
+		background: var(--color-odd);
 	}
 
 	.entry:nth-child(even) {
-	    background: var(--color-even);
+		background: var(--color-even);
 	}
 </style>
